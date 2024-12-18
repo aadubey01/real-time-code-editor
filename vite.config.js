1f7e028ecd473f7d245f6ce5b872d291ed1f7cec
Aakriti Dubey <aakritidubey51@gmail.com>
2024-12-19 02:47:26 +0530

    fix merge conflict in vite.conflict.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,15 +9,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-<<<<<<< HEAD
-   
   },
-  build:{
-=======
-    
+  build: {
+    outDir: "dist", // Specify the output directory for the build
   },
-   build:{
->>>>>>> 2e65208d064ded0e2f403ef22eb16d968a64afa6
-    outDir:'dist',
-  }
-})
+});
