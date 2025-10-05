e20459c23a00c1950dc1d1e28a727a2a2b3161be
Aakriti Dubey <91027094+aadubey01@users.noreply.github.com>
2025-10-05 14:22:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -91,4 +91,4 @@ io.on('connection',(socket)=>{
 
 
 const PORT= process.env.PORT || 5000;
-server.listen(PORT,()=>console.log(`served at http://localhost:${PORT}`))
+server.listen(PORT,'0.0.0.0',()=>console.log(`served at http://localhost:${PORT}`))
